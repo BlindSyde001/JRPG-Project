c294dd22520d18bd327af0a6fa9bbb3cdf4c8809
Samuel Jones <samuel.jones@live.com.au>
2020-02-10 16:22:45 +1100

    Work on spawning characters and statemachine

--- Assets/Scripts/Stats and AI Scripts/Players/PartyMember.cs
+++ Assets/Scripts/Stats and AI Scripts/Players/PartyMember.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PartyMember : BaseStats
 {
+    //VARIABLES
+
+    // What will be displayed in the Battle UI
+    #region In Battle UI Variables 
+    public Sprite characterPortrait;        // Portrait image displayed in UI
+    public int currentLimit;               // Limit amount of charge for special move
+    private int maxLimit;                  // Amount needed to use Limit. Will be 1 and any charge variable is normalized
+    #endregion
 
     //METHODS
     public override void Die()
--- Assets/Scripts/System Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/System Scripts/Battle/BattleManager.cs
@@ -9,10 +9,12 @@ public class BattleManager : MonoBehaviour
     //VARIABLES
     #region Reference Variables
     private GameManager _gameManager;
-    public List<GameObject> _CharacterPanel;
+    public List<GameObject> _CharacterPanels;
+    public List<GameObject> _Members;
     #endregion
     #region Positions
-    public List<GameObject> heroPos;
+    public List<GameObject> heroPosFront;
+    public List<GameObject> heroPosBack;
     public List<GameObject> enemyPos;
     #endregion
 
@@ -30,28 +32,31 @@ public class BattleManager : MonoBehaviour
     }
 
     //METHODS
+    #region Start Of Game
     private void SpawnCharacters()
     {
         // Instantiate selected heroes in party lineup
         for(int i = 0; i < _gameManager.partyLineup.Count; i++)      // Cycle through party member list
         {
             if(_gameManager.partyLineup[i] != "")                    // Check if there is a party member in that slot
-            {
+            {   
                 if(_gameManager.positionFront[i] == true)
                 {
-                    Instantiate(Resources.Load(_gameManager.partyLineup[i + (i - 1)]),    //HERO
-                        new Vector3(heroPos[i + (i - 1)].transform.position.x,            //POSITION
-                        heroPos[i + (i - 1)].transform.position.y + 0.5f,
-                        heroPos[i + (i - 1)].transform.position.z),
-                        heroPos[i + (i - 1)].transform.rotation);                         //ROTATION
+                    GameObject _Hero = Instantiate(Resources.Load(_gameManager.partyLineup[i]) as GameObject,      //HERO
+                        new Vector3(heroPosFront[i].transform.position.x,         //POSITION
+                        heroPosFront[i].transform.position.y + 0.5f,
+                        heroPosFront[i].transform.position.z),
+                        heroPosFront[i].transform.rotation);                      //ROTATION
+                    _Members.Add(_Hero);
                 }
                 else if(_gameManager.positionFront[i] == false)
                 {
-                    Instantiate(Resources.Load(_gameManager.partyLineup[2 * i]),      //HERO
-                        new Vector3(heroPos[2 * i].transform.position.x,              //POSITION
-                        heroPos[2 * i].transform.position.y + 0.5f,
-                        heroPos[2 * i].transform.position.z),
-                        heroPos[2 * i].transform.rotation);                           //ROTATION
+                    GameObject _Hero = Instantiate(Resources.Load(_gameManager.partyLineup[i]) as GameObject,      //HERO
+                        new Vector3(heroPosBack[i].transform.position.x,          //POSITION
+                        heroPosBack[i].transform.position.y + 0.5f,
+                        heroPosBack[i].transform.position.z),
+                        heroPosBack[i].transform.rotation);                       //ROTATION
+                    _Members.Add(_Hero);
                 }
             }
         }
@@ -71,12 +76,17 @@ public class BattleManager : MonoBehaviour
     }
     private void InstantiateUI()
     {
-        for(int i = 0; i < _gameManager.partyLineup.Count; i++)
+        for(int i = 0; i < _gameManager.partyLineup.Count; i++)         // Cycle trhough Party List
         {
-            if(_gameManager.partyLineup[i] != "")
+            if(_gameManager.partyLineup[i] != "")                       // If there is a party member
             {
-
+                // Set Name CharacterPanels[i].Find("Hero Image").Sprite = 
+                // Set Image
+                // Set HP UI text and visual
+                // Set MP Ui Text and visual
+                // Set Limit visual
             }
         }
     }
+    #endregion
 }
