989a506b04856bf0baecb594170d907a68c40a8a
dfgdf <aaaa@aaaa.aaa>
2020-03-09 19:09:30 +1100

    UI Interactive Button

--- Assets/Scripts/Battle Scripts/BattleUIController.cs
+++ Assets/Scripts/Battle Scripts/BattleUIController.cs
@@ -10,19 +10,33 @@ public class BattleUIController : MonoBehaviour
     //VARIABLES
     private BattleManager _BM;
     private GameManager _GM;
-    
+    public EventSystem _ES;
+    public GameObject testSelect;
+    public GameObject firstMagicButton;
+
     public GameObject messageBox;                 // For any messages needed to be written to player i.e victory/defeat, speech, etc
     public TextMeshProUGUI messageText;           // Text component of above
 
     #region Navigate UI Variables
-    public GameObject _CommandPanel;              // Panel which contains all the player input commands
-    public List<GameObject> EnemyUIButton;        // List of the buttons used to target enemies in battle
+    public List<Button> _CommandPanelButtons;     // Inputs for the player
+    public List<GameObject> EnemyTargets;        // List of the buttons used to target enemies in battle
+    public GameObject _EnemyTargetPanel;
+    public GameObject _MagicPanel;
     public BasePartyMember chosenHero;            // Currently selected hero, to control actions
     public int cycleButtonInput;
+    bool canAttack = false;
 
     public string action;
     public int _SpellID;
     public BaseStats targetForAction;
+
+    [Header("Button Colours")]
+    public Color normalColorActive;
+    public Color pressedColorActive;
+    public Color highlightedColorActive;
+    public Color normalColorInactive;
+    public Color pressedColorInactive;
+    public Color highlightedColorInactive;
     #endregion
 
     //UPDATES
@@ -37,15 +51,17 @@ public class BattleUIController : MonoBehaviour
         chosenHero = _BM._ActivePartyMembers[0];                   // Set first hero to control through UI
         _BM._CharacterPanels[0].transform.Find("Selected Panel").gameObject.SetActive(true);
         SetEnemyTargets();                                         // Set UI for enemies you can target
+        ToggleCommandButtons(false);
     }
     private void Update()
     {
-        if(chosenHero._ActionBarAmount >= 100)
+        if (chosenHero._ActionBarAmount >= 100)
         {
-            // Can use UI to input commands
-        } else
+            ToggleCommandButtons(true); // Can use UI to input commands
+        }
+        else
         {
-            // Cannot use UI to input commands
+            ToggleCommandButtons(false); // Cannot use UI to input commands
         }
     }
     private void LateUpdate()
@@ -77,19 +93,76 @@ public class BattleUIController : MonoBehaviour
     public void SetEnemyTargets()
     {
         // Set the buttons of targettable enemies. first turns them all off then cycle through how many and turn on the buttons to match.
-        foreach(GameObject a in EnemyUIButton)
+        foreach (GameObject a in EnemyTargets)
         {
             a.SetActive(false);
         }
         for (int i = 0; i < _BM._ActiveEnemies.Count; i++)
         {
-            EnemyUIButton[i].SetActive(true);
-            EnemyUIButton[i].GetComponentInChildren<TextMeshProUGUI>().text = _BM._ActiveEnemies[i].CharacterName;
+            EnemyTargets[i].SetActive(true);
+            EnemyTargets[i].GetComponentInChildren<TextMeshProUGUI>().text = _BM._ActiveEnemies[i].CharacterName;
         }
     }
     public void SetAvailableSpells()
     {
 
+    }
+
+    public void ToggleCommandButtons(bool isActive)
+    {
+        canAttack = isActive;   //Sets us to be able to use the attacke button as in the function AttackTest
+        _ES.firstSelectedGameObject = testSelect;
+        foreach (Button a in _CommandPanelButtons)
+        {
+            //Rather than using button.inactive, we are just changing the colors of the buttons to mimic being inactive
+            if (isActive)
+            {
+                ColorBlock colors = a.colors;
+                colors.normalColor = normalColorActive;
+                colors.highlightedColor = highlightedColorActive;
+                colors.selectedColor = highlightedColorActive;
+                colors.pressedColor = pressedColorActive;
+                a.colors = colors;
+            }
+            else
+            {
+                ColorBlock colors = a.colors;
+                colors.normalColor = normalColorInactive;
+                colors.highlightedColor = highlightedColorInactive;
+                colors.selectedColor = highlightedColorInactive;
+                colors.pressedColor = pressedColorInactive;
+                a.colors = colors;
+            }
+        }
+    }
+    public void DoAttack()
+    {
+        SetOffAllPanels();
+        if (canAttack)
+        {
+            _EnemyTargetPanel.SetActive(true);
+            EnemyTargets[0].GetComponent<Button>().Select();
+            ActionType("Attack");
+        }
+    }
+
+    public void DoMagic()
+    {
+        SetOffAllPanels();
+        if (canAttack)
+        {
+            //This is a bit buggy. Needs to be fixed properly
+            _MagicPanel.SetActive(true);
+            _ES.firstSelectedGameObject = firstMagicButton;   //Need to change to the magic buttons
+            EnemyTargets[0].GetComponent<Button>().Select();
+            ActionType("Magic");
+        }
+    }
+
+    void SetOffAllPanels()      //Turns off all our panels so they don't overlap
+    {
+        _EnemyTargetPanel.SetActive(false);
+        _MagicPanel.SetActive(false);
     }
     #region Test
     public void MessageOnScreen(string text)           // Display a message
